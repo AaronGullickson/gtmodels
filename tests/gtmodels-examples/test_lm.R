97b8be1c369952155ef2258fff719f988fcd7bfd
Aaron Gullickson <aarongullickson@gmail.com>
2024-02-14 15:33:55 -0800

    Change style for tests

--- tests/gtmodels-examples/test_lm.R
+++ tests/gtmodels-examples/test_lm.R
@@ -1,3 +1,4 @@
+library(gt)
 library(gtmodels)
 library(palmerpenguins)
 
@@ -6,21 +7,17 @@ model1 <- lm(mpg ~ hp, data = mtcars)
 model2 <- update(model1, . ~ . + disp + wt)
 model3 <- update(model2, . ~ . + as.factor(cyl))
 
-name_corr <- c(
-  "Intercept" = "Constant",
-  "hp" = "Horsepower",
-  "disp" = "Displacement (cu. in.)",
-  "wt" = "Weight (1000 lbs)",
-  "as.factor\\(cyl\\)6" = "6-cylinder",
-  "as.factor\\(cyl\\)8" = "8-cylinder",
-  "rsquared" = "R-squared",
-  "bic" = "BIC"
-)
+name_corr <- c("Intercept" = "Constant",
+               "hp" = "Horsepower",
+               "disp" = "Displacement (cu. in.)",
+               "wt" = "Weight (1000 lbs)",
+               "as.factor\\(cyl\\)6" = "6-cylinder",
+               "as.factor\\(cyl\\)8" = "8-cylinder",
+               "rsquared" = "R-squared",
+               "bic" = "BIC")
 
-gt_model(list(model1, model2, model3),
-  var_labels = name_corr,
-  summary_stats = c("rsquared", "bic")
-) |>
+gt_model(list(model1, model2, model3), var_labels = name_corr,
+         summary_stats = c("rsquared", "bic")) |>
   cols_label(model1 = "(1)", model2 = "(2)", model3 = "(3)") |>
   fmt_number(rows = c("Constant", "BIC"), decimals = 1) |>
   tab_source_note(md("*Notes:* Standard errors shown in parenthesis. Reference for cylinders is a 4-cylinder engine.")) |>
