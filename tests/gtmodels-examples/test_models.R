9c8797d7c47b00dd0ccdbb6c6890153d91158643
Aaron Gullickson <aarongullickson@gmail.com>
2024-02-18 09:59:55 -0800

    Rename test_lm.R; add glm test

--- tests/gtmodels-examples/test_models.R
+++ tests/gtmodels-examples/test_models.R
@@ -1,5 +1,8 @@
 library(gt)
 library(gtmodels)
+library(palmerpenguins)
+
+# lm ----------------------------------------------------------------------
 
 model1 <- lm(mpg ~ hp, data = mtcars)
 model2 <- update(model1, . ~ . + disp + wt)
@@ -26,6 +29,7 @@ gt_model(list(model1, model2, model3),
   tab_source_note(md("*Notes:* Standard errors shown in parenthesis.")) |>
   tab_options(table.width = "100%")
 
+# test beside
 gt_model(list(model1, model2, model3),
          var_labels = name_corr,
          digits=3,
@@ -36,3 +40,29 @@ gt_model(list(model1, model2, model3),
              model1_par = "(1)", model2_par = "(2)", model3_par = "(3)") |>
   fmt_number(rows = c("summary:bic"), decimals = 1) |>
   tab_source_note(md("*Notes:* Standard errors shown in parenthesis."))
+
+
+# glm ---------------------------------------------------------------------
+
+model1 <- glm(I(sex=="male")~body_mass_g+island, data=penguins, family=binomial)
+model2 <- update(model1, .~.+bill_length_mm)
+model3 <- update(model2, .~.+species)
+
+name_corr <- c("(Intercept)" = "Intercept",
+               "body_mass_g" = "Body mass (g)",
+               "bill_length_mm" = "Bill length (mm)",
+               "speciesChinstrap" = "Chinstrap",
+               "speciesGentoo" = "Gentoo",
+               "species" = "Species (ref. Adelie)",
+               "n" = "N",
+               "loglik" = "Log-likelihood",
+               "deviance" = "Deviance",
+               "pseudo_rsquared" = "Pseudo R-squared")
+
+gt_model(list(model1, model2, model3),
+         var_labels = name_corr,
+         omit_var = "island",
+         summary_stats = c("loglik", "deviance", "pseudo_rsquared")) |>
+  fmt_number(rows = c("summary:loglik", "summary:deviance"), decimals = 1) |>
+  tab_source_note(md("*Notes:* Standard errors shown in parenthesis. All models include island fixed effects"))
+
