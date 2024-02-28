d3b9a270cb105bc0048a1183ba9ac3c07f3d2f16
Aaron Gullickson <aarongullickson@gmail.com>
2024-02-28 14:18:48 -0800

    Add some basic tests

---
+++ tests/testthat/test-gt_model.R
@@ -0,0 +1,37 @@
+# create some models for testing
+
+# build some testing data
+model1 <- lm(mpg ~ hp, data = mtcars)
+model2 <- update(model1, . ~ . + disp + wt)
+model3 <- update(model2, . ~ . + as.factor(cyl))
+models <- list(model1, model2, model3)
+name_corr <- c("(Intercept)" = "Constant",
+               "hp" = "Horsepower",
+               "disp" = "Displacement (cu. in.)",
+               "wt" = "Weight (1000 lbs)",
+               "as.factor(cyl)" = "Number of cylinders (ref. 4-cylinder)",
+               "as.factor(cyl)6" = "6-cylinder",
+               "as.factor(cyl)8" = "8-cylinder",
+               "nobs" = "N",
+               "r.squared" = "R-squared",
+               "BIC" = "BIC")
+
+
+test_that("The output of a basic gt_model is as expected", {
+  tbl <- gt_model(models)
+  expect_s3_class(tbl, "gt_tbl")
+  tbl |> as_latex() |> expect_snapshot()
+})
+
+test_that("Additional summary statistics can be added", {
+  tbl <- gt_model(models, summary_stats = c("r.squared", "BIC"))
+  expect_equal(tbl$`_data`$variable[14:15],
+               c("summary:r.squared", "summary:BIC"))
+})
+
+test_that("Variable labels are correctly applied", {
+  tbl <- gt_model(models,
+                  var_labels = name_corr)
+  tbl |> as_latex() |> expect_snapshot()
+})
+
