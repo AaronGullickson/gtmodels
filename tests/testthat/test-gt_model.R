0137734374ae2586fa04a677ef304a2e61b39505
Aaron Gullickson <aarongullickson@gmail.com>
2024-02-28 14:32:05 -0800

    Add argument tests

--- tests/testthat/test-gt_model.R
+++ tests/testthat/test-gt_model.R
@@ -16,7 +16,6 @@ name_corr <- c("(Intercept)" = "Constant",
                "r.squared" = "R-squared",
                "BIC" = "BIC")
 
-
 test_that("The output of a basic gt_model is as expected", {
   tbl <- gt_model(models)
   expect_s3_class(tbl, "gt_tbl")
@@ -29,9 +28,31 @@ test_that("Additional summary statistics can be added", {
                c("summary:r.squared", "summary:BIC"))
 })
 
+test_that("Rounding digits works as expected", {
+  tbl <- gt_model(models, digits = 2)
+  tbl |> as_latex() |> expect_snapshot()
+})
+
 test_that("Variable labels are correctly applied", {
-  tbl <- gt_model(models,
-                  var_labels = name_corr)
+  tbl <- gt_model(models, var_labels = name_corr)
+  tbl |> as_latex() |> expect_snapshot()
+})
+
+test_that("Variables can be omitted", {
+  tbl <- gt_model(models, omit_var = c("hp", "wt"))
+  expect_equal("hp" %in% tbl$`_data`$variable, FALSE)
+  expect_equal("wt" %in% tbl$`_data`$variable, FALSE)
+})
+
+test_that("Group rows can be added", {
+  tbl <- gt_model(models, groups = c("as.factor(cyl)"))
+  expect_equal("grp:as.factor(cyl)" %in% tbl$`_data`$variable, TRUE)
+})
+
+test_that("Parenthetical value can be changed", {
+  tbl <- gt_model(models, parenthetical_value = "statistic")
+  tbl |> as_latex() |> expect_snapshot()
+  tbl <- gt_model(models, parenthetical_value = "p.value")
   tbl |> as_latex() |> expect_snapshot()
 })
 
